b0cdde24aa777f534f9d900ecf2af63bc672fbc7
m1ras01 <221930@astanait.edu.kz>
2023-05-14 20:17:39 +0600

    MyTesting clas

---
+++ src/MyTestingClass.java
@@ -0,0 +1,15 @@
+public class MyTestingClass {
+    private String name;
+    private int ID;
+
+    public MyTestingClass(String name, int ID){
+        this.name = name;
+        this.ID = ID;
+    }
+
+    public int getID(){
+        return ID;
+    }
+
+
+}
